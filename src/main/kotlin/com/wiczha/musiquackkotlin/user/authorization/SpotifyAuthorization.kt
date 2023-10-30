97a6a975724d92956cfcaeba407df173b7514ced
Wictor Zhao <wictor_x@hotmail.com>
2023-10-30 21:28:31 +0100

    Add more scopes

--- src/main/kotlin/com/wiczha/musiquackkotlin/user/authorization/SpotifyAuthorization.kt
+++ src/main/kotlin/com/wiczha/musiquackkotlin/user/authorization/SpotifyAuthorization.kt
@@ -7,29 +7,27 @@ import se.michaelthelin.spotify.requests.authorization.authorization_code.Author
 
 class SpotifyAuthorization(spotifyCallbackUri: String) {
 
-    private val redirectUri = SpotifyHttpManager.
-    makeUri(spotifyCallbackUri)
-    fun getSpotifyBuilder(clientID: String, clientSecret: String): SpotifyApi
-        = SpotifyApi.builder()
+    private val redirectUri = SpotifyHttpManager.makeUri(spotifyCallbackUri)
+    fun getSpotifyBuilder(clientID: String, clientSecret: String): SpotifyApi = SpotifyApi.builder()
         .setClientId(clientID)
         .setClientSecret(clientSecret)
         .setRedirectUri(redirectUri)
         .build()
 
-    fun authorizationCodeUriRequest(spotifyApi: SpotifyApi): AuthorizationCodeUriRequest
-        = spotifyApi.authorizationCodeUri()
-        //TODO: FIX STATE
-        .state("x4xkmn9pu3jsdadsasa")
-        .scope("user-read-email,user-read-private,playlist-read-private,playlist-read-collaborative")
-        .show_dialog(true)
-        .build()
+    fun authorizationCodeUriRequest(spotifyApi: SpotifyApi): AuthorizationCodeUriRequest =
+        spotifyApi.authorizationCodeUri()
+            //TODO: FIX STATE
+            .state("x4xkmn9pu3jsdadsasa")
+            .scope("user-read-email,user-read-private,playlist-read-private,playlist-read-collaborative,user-read-playback-state,user-read-currently-playing,user-modify-playback-state,app-remote-control")
+            .show_dialog(true)
+            .build()
 
-    fun buildAuthorizationCode( spotifyApi: SpotifyApi, code: String ): AuthorizationCodeRequest
-            = spotifyApi.authorizationCode(code).build()
+    fun buildAuthorizationCode(spotifyApi: SpotifyApi, code: String): AuthorizationCodeRequest =
+        spotifyApi.authorizationCode(code).build()
 
     fun tokenAuthorization(accessToken: String?): SpotifyApi =
         SpotifyApi.builder().setAccessToken(accessToken).build()
 
-    fun refreshTokenAuthorization(refreshToken: String?, clientID: String, clientSecret: String): SpotifyApi
-        = SpotifyApi.Builder().setClientId(clientID).setClientSecret(clientSecret).setRefreshToken(refreshToken).build()
+    fun refreshTokenAuthorization(refreshToken: String?, clientID: String, clientSecret: String): SpotifyApi =
+        SpotifyApi.Builder().setClientId(clientID).setClientSecret(clientSecret).setRefreshToken(refreshToken).build()
 }
